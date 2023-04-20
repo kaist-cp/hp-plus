0c7f20ad15911f4a247cc70a7efd9599ad3cf9cb
Jeonghyeon Kim <powergee101@gmail.com>
2023-04-20 16:07:26 +0900

    Update README and rename the benchmark suite

--- archive.sh
+++ archive.sh
@@ -3,7 +3,9 @@ git submodule update --init --recursive
 cargo clean
 cd crossbeam-ebr; cargo clean; cd ..
 cd crossbeam-pebr; cargo clean; cd ..
+cd nbr-rs; cargo clean; cd ..
+cd cdrc-rs; cargo clean; cd ..
 
-git-archive-all pebr-benchmark.zip
+git-archive-all hp-plus-benchmark.zip
 
 rm -- "$0"
